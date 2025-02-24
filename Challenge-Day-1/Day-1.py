e059b3cfbf3c79b71bd7bb2e4485c10aa7a4df5d
Umer-Ali7 <ua472443@gmail.com>
2025-02-25 00:11:08 +0500

    Daily-Python-Challenge-Day-1

---
+++ Challenge-Day-1/Day-1.py
@@ -0,0 +1,6 @@
+
+print("\n---------------------------------Python Challenge Day 1 🐍---------------------------------\n")
+
+name = "Umer Ali"
+
+print(f"{name}: Hello, Python Learners! 🚀 Today is the fisrt step in our coding journey.\n")
